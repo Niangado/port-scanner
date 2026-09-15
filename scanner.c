038e5cbe21742e29513f72b4f9366a03f9bdb063
Niangado <Niangado@users.noreply.github.com>
2026-09-14 23:43:49 -0400

    Added concurrent scanning with threads and mutex protection on critical section

--- scanner.c
+++ scanner.c
@@ -5,39 +5,70 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
+ size_t N;
+ pthread_mutex_t print_lock = PTHREAD_MUTEX_INITIALIZER;
+typedef struct __holder_t{
+	int port;
+	char* IP;
+} holder_t;
+
+void* scan_port(void* arg){
+	holder_t m = *(holder_t*)arg;
+	int port = m.port;
+
+	struct sockaddr_in addr;
+     memset(&addr, 0, sizeof(addr));
+     addr.sin_family = AF_INET;
+     addr.sin_addr.s_addr = inet_addr(m.IP);
+
+	int sock = socket(AF_INET,SOCK_STREAM,0);
+	
+	if (sock == -1){
+		perror("socket");
+			return NULL;}
+		
+	addr.sin_port = htons(port);
+	int result = connect(sock, (struct sockaddr*) &addr, sizeof(addr));
+		if (result == 0){
+			pthread_mutex_lock(&print_lock);
+			printf("Port %d: open\n", port);
+			pthread_mutex_unlock(&print_lock);
+		}
+	
+		close(sock);
+	return NULL;
+}
+
+
 int main(int argc, char* argv[]){
 	if (argc < 4){
 		fprintf(stderr, "Usage: %s <target-ip> <start-port> <end-port>\n",argv[0]);
 		return 1;
 	}
+
 	if (inet_addr(argv[1]) == INADDR_NONE){
 		fprintf(stderr, "Invalid IP Address: %s\n", argv[1]);
 		return 1;
 	}
-	 struct sockaddr_in addr;
-     memset(&addr, 0, sizeof(addr));
-     addr.sin_family = AF_INET;
-     addr.sin_addr.s_addr = inet_addr(argv[1]);
 
 	int start = atoi(argv[2]);
 	int end = atoi(argv[3]);
 
-	if (start > end || end > 65535 || start < 1){
-		fprintf(stderr, "Invalid Port Range: %d %d\n",start,end);
-		return 1;
+	N = end - start + 1;
+	holder_t test[N];
+	pthread_t threads[N];
+	
+	for (size_t counter = 0; counter < N ; ++counter){
+		test[counter].port = start + counter;
+		test[counter].IP = argv[1];
+		pthread_create(&threads[counter], NULL, scan_port, &test[counter]);
 	}
 
-	for (int port = start; port  <= end; ++port){
-		int sock = socket(AF_INET,SOCK_STREAM,0);
-			if (sock == -1){
-				perror("socket");
-			continue;
-			} 
-		addr.sin_port = htons(port);
+	for(size_t i = 0; i < N; ++i){pthread_join(threads[i],NULL);}
+
+
 	
-		int result = connect(sock, (struct sockaddr*) &addr, sizeof(addr));
-		if (result == 0){printf("Port %d: open\n", port);}
-		close(sock);
-	}
 	return 0;
 }
